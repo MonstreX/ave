948461924ffcc7be50dbba01cf8a115495061e38
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-11-08 13:20:33 +0500

    Refactor: Reimplement Bootstrap Toggle with correct sliding handle design
    - Replace previous two-button implementation with proper bootstrap-toggle style
    - Implement sliding toggle-handle that moves left/right to show On/Off states
    - Use correct bootstrap-toggle CSS structure (toggle-group, toggle-on/off, toggle-handle)
    - Simplify view code - remove unnecessary wrappers
    - Simplify JavaScript with toggle.classList.toggle() approach
    - Add complete bootstrap-toggle.css v2.2.0 compatible SCSS
    - Support lg, sm, xs button sizes
    - All 127 tests passing

--- resources/views/components/forms/fields/toggle/bootstrap.blade.php
+++ resources/views/components/forms/fields/toggle/bootstrap.blade.php
@@ -1,112 +1,62 @@
-{{-- Bootstrap-style toggle with two buttons (On/Off) --}}
 @php
-    $labelText = $label ?? $field->getLabel();
-    $helpText = ($help ?? null) ?: $field->getHelpText();
-    $isRequired = $required ?? $field->isRequired();
-    $isDisabled = $disabled ?? false;
-    $isReadonly = $readonly ?? false;
-    $isChecked = $checked ?? ($value ?? false);
-
-    // Get on/off labels from field
+    $id = 'toggle_' . uniqid();
+    $isChecked = $checked ?? ($value ?? false) ?? old($key);
     $onLabel = $field->getOnLabel() ?? 'On';
     $offLabel = $field->getOffLabel() ?? 'Off';
-
-    $uniqueId = $key . '_' . uniqid();
 @endphp
 
-<div class="form-field form-field-toggle-bootstrap @if($hasError) has-error @endif">
-    @if($labelText)
+<div class="form-field @if($hasError) has-error @endif">
+    @if($label ?? $field->getLabel())
         <label class="form-label">
-            {{ $labelText }}
-            @if($isRequired)
-                <span class="required">*</span>
-            @endif
+            {{ $label ?? $field->getLabel() }}
+            @if($required ?? $field->isRequired())<span class="required">*</span>@endif
         </label>
     @endif
 
-    <div class="toggle-bootstrap-wrapper">
-        {{-- Hidden input for form submission --}}
-        <input
-            type="hidden"
-            name="{{ $key }}"
-            value="0"
-            class="toggle-bootstrap-hidden"
-        >
-
-        {{-- Actual checkbox (hidden visually) --}}
-        <input
-            type="checkbox"
-            id="{{ $uniqueId }}"
-            class="toggle-bootstrap-input"
-            @if($isChecked || old($key, false)) checked @endif
-            @if($isRequired) required @endif
-            @if($isDisabled) disabled @endif
-            @if($isReadonly) readonly @endif
-            {!! $attributes !!}
-        >
-
-        {{-- Visual toggle buttons --}}
-        <div class="toggle-bootstrap-group" data-toggle-id="{{ $uniqueId }}">
-            <label class="toggle-bootstrap-btn toggle-bootstrap-on">
-                {{ $onLabel }}
-            </label>
-            <label class="toggle-bootstrap-btn toggle-bootstrap-off">
-                {{ $offLabel }}
-            </label>
-            <span class="toggle-bootstrap-handle"></span>
+    <div class="toggle btn btn-primary @if(!$isChecked) off @endif"
+         data-toggle="toggle"
+         data-on="{{ $onLabel }}"
+         data-off="{{ $offLabel }}">
+        <input type="checkbox"
+               id="{{ $id }}"
+               name="{{ $key }}"
+               class="toggleswitch"
+               @if($isChecked) checked @endif
+               @if($required ?? $field->isRequired()) required @endif
+               @if($disabled ?? false) disabled @endif
+               {!! $attributes !!}>
+        <div class="toggle-group">
+            <label class="btn btn-primary toggle-on">{{ $onLabel }}</label>
+            <label class="btn btn-default toggle-off @if($isChecked) active @endif">{{ $offLabel }}</label>
+            <span class="toggle-handle btn btn-default"></span>
         </div>
     </div>
 
     @if(!empty($errors))
         <div class="error-message">
-            @foreach($errors as $error)
-                <p>{{ $error }}</p>
-            @endforeach
+            @foreach($errors as $error) <p>{{ $error }}</p> @endforeach
         </div>
     @endif
 
-    @if(!empty($helpText))
-        <div class="help-text">{{ $helpText }}</div>
+    @if($help ?? $field->getHelpText())
+        <div class="help-text">{{ $help ?? $field->getHelpText() }}</div>
     @endif
 </div>
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleId = '{{ $uniqueId }}';
-    const checkbox = document.getElementById(toggleId);
-    const toggleGroup = document.querySelector('[data-toggle-id="' + toggleId + '"]');
-    const onBtn = toggleGroup.querySelector('.toggle-bootstrap-on');
-    const offBtn = toggleGroup.querySelector('.toggle-bootstrap-off');
+    const checkbox = document.getElementById('{{ $id }}');
+    const toggle = checkbox.closest('.toggle');
+    const updateState = () => {
+        toggle.classList.toggle('off', !checkbox.checked);
+        toggle.querySelector('.toggle-on').classList.toggle('active', !checkbox.checked);
+        toggle.querySelector('.toggle-off').classList.toggle('active', checkbox.checked);
+    };
 
-    function updateToggleState() {
-        if (checkbox.checked) {
-            onBtn.classList.add('active');
-            offBtn.classList.remove('active');
-        } else {
-            onBtn.classList.remove('active');
-            offBtn.classList.add('active');
-        }
-    }
-
-    // Initialize state
-    updateToggleState();
-
-    // Handle checkbox change
-    checkbox.addEventListener('change', function() {
-        updateToggleState();
-    });
-
-    // Handle button clicks
-    onBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        checkbox.checked = true;
-        checkbox.dispatchEvent(new Event('change'));
-    });
-
-    offBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        checkbox.checked = false;
-        checkbox.dispatchEvent(new Event('change'));
-    });
+    updateState();
+    checkbox.addEventListener('change', updateState);
+    toggle.querySelector('.toggle-on').addEventListener('click', e => (e.preventDefault(), checkbox.checked = true, checkbox.dispatchEvent(new Event('change'))));
+    toggle.querySelector('.toggle-off').addEventListener('click', e => (e.preventDefault(), checkbox.checked = false, checkbox.dispatchEvent(new Event('change'))));
+    toggle.querySelector('.toggle-handle').addEventListener('click', e => (e.preventDefault(), checkbox.checked = !checkbox.checked, checkbox.dispatchEvent(new Event('change'))));
 });
 </script>
