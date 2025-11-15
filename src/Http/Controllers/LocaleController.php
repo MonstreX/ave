5b6f4f4fc9e7a7d6d3d44420c94564c105587157
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-11-15 18:32:42 +0500

    Fix LocaleController: use Illuminate\Routing\Controller

--- src/Http/Controllers/LocaleController.php
+++ src/Http/Controllers/LocaleController.php
@@ -3,6 +3,7 @@
 namespace Monstrex\Ave\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
