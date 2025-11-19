433b6dbcd716b3d6241d76db6a68bdda1fb43a28
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-11-19 19:03:38 +0500

    Enhance Col component with automatic responsive classes
    Modified Col::make() to automatically generate mobile-first responsive classes:
    - Col::make(6) now generates 'col-12 col-md-6' (full on mobile, half on desktop)
    - Col::make(4) now generates 'col-12 col-md-4' (full on mobile, third on desktop)
    - Col::make(12) generates 'col-12' (full width on all devices)
    
    This ensures all Resource forms are automatically responsive without manual
    class specification, following Bootstrap mobile-first design principles.
    
    Affects all Resources recently converted from Div to Row/Col structure.

--- src/Core/Components/Col.php
+++ src/Core/Components/Col.php
@@ -6,26 +6,34 @@ namespace Monstrex\Ave\Core\Components;
  * Col - Factory for Div with Bootstrap column class
  *
  * Convenient wrapper for creating column containers in Bootstrap grid layout.
- * Automatically applies class="col-{span}" for responsive column sizing.
+ * Automatically applies responsive classes for adaptive design.
  *
  * Bootstrap grid spans from 1-12.
  *
  * Example:
  *   Row::make()->schema([
- *       Col::make(6)->schema([TextInput::make('name')]),      // Half width
- *       Col::make(6)->schema([TextInput::make('email')]),     // Half width
+ *       Col::make(6)->schema([TextInput::make('name')]),      // col-12 col-md-6
+ *       Col::make(6)->schema([TextInput::make('email')]),     // col-12 col-md-6
  *   ])
  *
- *   Col::make()    // Defaults to col-12 (full width)
- *   Col::make(3)   // col-3 (quarter width)
- *   Col::make(4)   // col-4 (third width)
- *   Col::make(8)   // col-8 (two thirds width)
+ *   Col::make()    // col-12 (full width on all devices)
+ *   Col::make(3)   // col-12 col-md-3 (full on mobile, quarter on desktop)
+ *   Col::make(4)   // col-12 col-md-4 (full on mobile, third on desktop)
+ *   Col::make(6)   // col-12 col-md-6 (full on mobile, half on desktop)
+ *   Col::make(12)  // col-12 (full width on all devices)
  */
 class Col
 {
     public static function make(int $span = 12, string $additionalClasses = ''): Div
     {
-        $classes = "col-{$span}";
+        // For spans < 12, add responsive classes: col-12 col-md-{span}
+        // This makes fields full-width on mobile, specified width on desktop
+        if ($span < 12) {
+            $classes = "col-12 col-md-{$span}";
+        } else {
+            $classes = "col-{$span}";
+        }
+
         if ($additionalClasses) {
             $classes .= ' ' . $additionalClasses;
         }
