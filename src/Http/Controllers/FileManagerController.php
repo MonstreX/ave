f318f69f03f3905545184c793ca7554898fbe724
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-11-22 13:05:12 +0500

    Add authorization to FileManagerController
    - Check file-manager.viewAny for index, list, read methods
    - Check file-manager.create for save, createDirectory, upload methods
    - Check file-manager.delete for delete, rename methods
    - Return 403 if user lacks required permission
    - Uses AccessManager for permission checks

--- src/Http/Controllers/FileManagerController.php
+++ src/Http/Controllers/FileManagerController.php
@@ -6,12 +6,14 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
+use Monstrex\Ave\Admin\Access\AccessManager;
 use Monstrex\Ave\Services\FileManagerService;
 
 class FileManagerController extends Controller
 {
     public function __construct(
-        private FileManagerService $fileManager
+        private FileManagerService $fileManager,
+        private AccessManager $accessManager
     ) {
     }
 
@@ -20,6 +22,12 @@ class FileManagerController extends Controller
      */
     public function index(Request $request): View
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'viewAny'),
+            403,
+            'Unauthorized access to File Manager'
+        );
+
         $path = $request->get('path') ?? '';
         $data = $this->fileManager->list($path);
 
@@ -34,6 +42,12 @@ class FileManagerController extends Controller
      */
     public function list(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'viewAny'),
+            403,
+            'Unauthorized access to File Manager'
+        );
+
         $path = $request->get('path') ?? '';
         $data = $this->fileManager->list($path);
 
@@ -45,6 +59,12 @@ class FileManagerController extends Controller
      */
     public function read(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'viewAny'),
+            403,
+            'Unauthorized access to File Manager'
+        );
+
         $path = $request->get('path') ?? '';
         $data = $this->fileManager->read($path);
 
@@ -60,6 +80,12 @@ class FileManagerController extends Controller
      */
     public function save(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'create'),
+            403,
+            'Unauthorized to create/edit files'
+        );
+
         $request->validate([
             'path' => 'required|string',
             'content' => 'present|string',
@@ -82,6 +108,12 @@ class FileManagerController extends Controller
      */
     public function createDirectory(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'create'),
+            403,
+            'Unauthorized to create directories'
+        );
+
         $request->validate([
             'path' => 'nullable|string',
             'name' => 'required|string|max:255',
@@ -104,6 +136,12 @@ class FileManagerController extends Controller
      */
     public function upload(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'create'),
+            403,
+            'Unauthorized to upload files'
+        );
+
         $request->validate([
             'path' => 'nullable|string',
             'file' => 'required|file',
@@ -126,6 +164,12 @@ class FileManagerController extends Controller
      */
     public function delete(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'delete'),
+            403,
+            'Unauthorized to delete files'
+        );
+
         $request->validate([
             'path' => 'required|string',
         ]);
@@ -144,6 +188,12 @@ class FileManagerController extends Controller
      */
     public function rename(Request $request): JsonResponse
     {
+        abort_unless(
+            $this->accessManager->allows(ave_auth_user(), 'file-manager', 'delete'),
+            403,
+            'Unauthorized to rename files'
+        );
+
         $request->validate([
             'path' => 'required|string',
             'name' => 'required|string|max:255',
